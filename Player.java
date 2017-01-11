9c6972f338d78b125efabab27077b60615e4b1f2
shivasuryan-vummidi <svummidi@stuy.edu>
2017-01-11 10:16:18 -0500

    MAJOR CHANGE, ADDED ALGOS

--- Player.java
+++ Player.java
@@ -1,7 +1,9 @@
 public abstract class Player {
 
     protected tokens;
-    
+    protected name;
+    protected token_name;
+
     void drop_token();
 
     void is_win();
--- Woo.java
+++ Woo.java
@@ -1,13 +1,15 @@
+import java.io.*;
+import java.util.*;
 public class Woo {
     
-    private String[][] _board;
+    private char[][] _board;
     private boolean gameOver;
 
     private InputStreamReader isr;
     private BufferedReader in;
 
     public void create_board() {
-	_board = new String[8][8];
+	_board = new char[8][8];
     }
 
     public void check_winner(){
@@ -22,6 +24,31 @@ public class Woo {
 	in = new BufferedReader( isr );
     }
 
+    public boolean is_column_full(int column){
+        return(_board[column][0] != ' ');
+    }
     
-
+    public void drop(int column, char token_name){
+	for(int x = 7; x > 0; x--){
+	    if(_board[column][x] == ' '){
+		_board[column][x] = token_name;
+		return;
+	    }
+	}
+    }
+    public String printBoard(){
+	String s = "";
+	for(int y = 0; y < 7; y++){
+	    for(int x = 0; x < 7; x++){
+		s += _board[y][x] + " ";
+	    }
+	    s+= "\n";
+	}
+	return s;
+    }
+    public static void main(String[] args){
+	Woo a = new Woo();
+	System.out.println( a.printBoard());
+	
+    }
 }
