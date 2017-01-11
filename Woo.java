920de2091b5b0c331a3598c01b61ad8e5637c986
EdwinMok <emok@stuy.edu>
2017-01-11 10:00:13 -0500

    Added default constructor to driver file

--- Woo.java
+++ Woo.java
@@ -1,9 +1,27 @@
 public class Woo {
     
-    protected String[][] _board;
+    private String[][] _board;
+    private boolean gameOver;
+
+    private InputStreamReader isr;
+    private BufferedReader in;
 
     public void create_board() {
 	_board = new String[8][8];
     }
+
+    public void check_winner(){
+	
+    }
+
+    //DEFAULT CONSTRUCTOR
+    public Woo(){
+        create_board();
+	gameOver = false;
+	isr = new InputStreamReader( System.in );
+	in = new BufferedReader( isr );
+    }
+
+    
 
 }
