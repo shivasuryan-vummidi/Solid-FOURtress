1b03e43fbcc3bdf281b70653b033d80f36d86b50
EdwinMok <emok@stuy.edu>
2017-01-12 21:42:55 -0500

    Added Computer.java, the PC mode and the first step towards letting PC play with the User.

---
+++ Computer.java
@@ -0,0 +1,82 @@
+public class Computer extends Player {
+
+    public Computer(){
+	tokens = 32;
+	name = "Computer";
+	token_name = "C";
+    }
+
+    public void drop_token(int column, Woo w){
+	if (w.is_column_full(column)){
+	    System.out.println("ERROR: Please try again. Column " + column + " is full.");
+	}
+	else {
+	    w.drop(column, this);
+	}
+    }
+
+    public boolean is_win(Woo w){
+	String win = "CCCC";
+	return hasSubString(getLastRowString(w),win) ||
+	    hasSubString(getLastColumnString(w),win) ||
+	    hasSubString(getLastRightDiagonalString(w),win) ||
+	    hasSubString(getLastLeftDiagonalString(w),win);
+    }
+
+    public String getLastRowString(Woo w){
+	String s = "";
+	for (int x = 0; x < 8; x++){
+	    s += w._board[_lastRow][x];
+	}
+	return s;
+    }
+
+    public String getLastColumnString(Woo w){
+	String s = "";
+	for (int x = 0; x < 8; x++){
+	    s += w._board[x][_lastColumn];
+	}
+	return s;
+    }
+
+    public String getLastRightDiagonalString(Woo w){
+	int startingRow = _lastRow;
+	int startingColumn = _lastColumn;
+	for (int x = 0; x < 8; x++){
+	    if (startingRow != 7 && startingColumn != 0){
+		startingRow += 1;
+		startingColumn -= 1;
+	    }
+	}
+	String s = "";
+	for (int x = 0; x < 8; x++){
+	    if (startingRow != 0 && startingColumn != 7){
+		s += w._board[startingRow][startingColumn];
+		startingRow -= 1;
+		startingColumn += 1;
+	    }
+	}
+	return s;
+    }
+
+    public String getLastLeftDiagonalString(Woo w){
+	int startingRow = _lastRow;
+	int startingColumn = _lastColumn;
+	for (int x = 0; x < 8; x++){
+	    if (startingRow != 0 && startingColumn != 0){
+		startingRow -= 1;
+		startingColumn -= 1;
+	    }
+	}
+	String s = "";
+	for (int x = 0; x < 8; x++){
+	    if (startingRow != 7 && startingColumn != 7){
+		s += w._board[startingRow][startingColumn];
+		startingRow += 1;
+		startingColumn += 1;
+	    }
+	}
+	return s;
+    }
+    
+}
